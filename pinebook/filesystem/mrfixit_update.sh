258c260975b7c79d64d7d1cd3f2a37625000673d
mrfixit3000 <mrfixit2001@gmail.com>
2019-10-04 16:25:41 -0400

    v1.1 updates

--- pinebook/filesystem/mrfixit_update.sh
+++ pinebook/filesystem/mrfixit_update.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-# This file was placed here as a test to confirm the update script runs
-echo "No Filesystem Updates for version 1.0"
+echo "Updating U-Boot to Improve Support for booting other distros..."
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+SYSPART=$(findmnt -n -o SOURCE /)
+if echo $SYSPART | grep -qE 'p[0-9]$' ; then
+	DEVID=$(echo $SYSPART | sed -e s+'p[0-9]$'+''+)
+else
+	DEVID=$(echo $SYSPART | sed -e s+'[0-9]$'++)
+fi
+
+echo Identified $DEVID as device to flash uboot to...
+
+if [ -f $DIR/idbloader.img ] ; then
+	echo "Upgrading idbloader.img..."
+	dd if=$DIR/idbloader.img of=$DEVID bs=32k seek=1 conv=fsync &>/dev/null
+fi
+if [ -f $DIR/uboot.img ] ; then
+	echo "Upgrading uboot.img..."
+	dd if=$DIR/uboot.img of=$DEVID bs=64k seek=128 conv=fsync &>/dev/null
+fi
+if [ -f $DIR/trust.img ] ; then
+	echo "Upgrading trust.img..."
+	dd if=$DIR/trust.img of=$DEVID bs=64k seek=192 conv=fsync &>/dev/null
+fi
