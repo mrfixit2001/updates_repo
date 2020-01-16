d60a08bce125905616352eb140a3895522baf6e4
mrfixit3000 <mrfixit2001@gmail.com>
2020-01-15 21:21:11 -0500

    v1.9

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This branch contains updates for version 1.8
+This branch contains updates for version 1.9
--- pinebook/filesystem/mrfixit_update.sh
+++ pinebook/filesystem/mrfixit_update.sh
@@ -21,6 +21,40 @@ if [[ $myver < 1.2 ]]; then
 fi
 
 if [[ $myver < 1.5 ]]; then
+	echo
+	echo "Updating Chromium..."
+	dpkg -i $DIR/chromium-codecs-ffmpeg-extra_78.0.3904.97-0ubuntu0.16.04.1_armhf.deb
+	dpkg -i $DIR/chromium-browser_78.0.3904.97-0ubuntu0.16.04.1_armhf.deb
+
+	# Update bootpartscript
+	mv $DIR/partbootscript.sh /usr/bin
+	chmod +x /usr/bin/partbootscript.sh
+
+	# Disable wifi power management
+	mv $DIR/rc.local /etc
+	chmod +x /etc/rc.local
+fi
+
+if [[ $myver < 1.6 ]]; then
+	# Fix firefox graphics acceleration
+	for file in /home/*/.mozilla/firefox/*/prefs.js; do
+		echo 'user_pref("gfx.xrender.enabled", true);' >> "$file"
+	done
+fi
+
+if [[ $myver < 1.8 ]]; then
+	echo "Installing Chromium Default Flags for Improved Performance..."
+	mv $DIR/default /etc/chromium-browser
+fi
+
+if [[ $myver < 1.9 ]]; then
+	# Leave the current kernel version's modules folder in place so that it can be used as the backup kernel
+	echo "Updating Kernel Modules to 4.4.210..."
+	KER="$(uname -r)"
+	find /lib/modules -mindepth 1 ! -regex '^/lib/modules/'$KER'\(/.*\)?' -delete
+	rm /lib/modules/4.4.210 -r
+	mv $DIR/4.4.210 /lib/modules
+
 	echo "Updating U-Boot..."
 	SYSPART=$(findmnt -n -o SOURCE /)
 	if echo $SYSPART | grep -qE 'p[0-9]$' ; then
@@ -42,38 +76,19 @@ if [[ $myver < 1.5 ]]; then
 		dd if=$DIR/trust.img of=$DEVID bs=64k seek=192 conv=fsync &>/dev/null
 	fi
 
-	echo
-	echo "Updating Chromium and Firefox..."
-	dpkg -i $DIR/chromium-codecs-ffmpeg-extra_78.0.3904.97-0ubuntu0.16.04.1_armhf.deb
-	dpkg -i $DIR/chromium-browser_78.0.3904.97-0ubuntu0.16.04.1_armhf.deb
-	dpkg -i $DIR/firefox_70.0.1+build1-0ubuntu0.16.04.1_armhf.deb
+	echo "Updating Firefox..."
+	dpkg -i $DIR/firefox_71.0+build5-0ubuntu0.16.04.1_armhf.deb
 
-	# Update bootpartscript
-	mv $DIR/partbootscript.sh /usr/bin
-	chmod +x /usr/bin/partbootscript.sh
-
-	# Disable wifi power management
-	mv $DIR/rc.local /etc
-	chmod +x /etc/rc.local
-fi
-
-if [[ $myver < 1.6 ]]; then
-	# Fix firefox graphics acceleration
+	echo "Fixing firefox sound..."
 	for file in /home/*/.mozilla/firefox/*/prefs.js; do
-		echo 'user_pref("gfx.xrender.enabled", true);' >> "$file"
+		echo 'user_pref("media.cubeb.backend", "pulse");' >> "$file"
 	done
-fi
 
-if [[ $myver < 1.8 ]]; then
-	# Leave the current kernel version's modules folder in place so that it can be used as the backup kernel
-	echo "Updating Kernel Modules to 4.4.207..."
-	KER="$(uname -r)"
-	find /lib/modules -mindepth 1 ! -regex '^/lib/modules/'$KER'\(/.*\)?' -delete
-	rm /lib/modules/4.4.207 -r
-	mv $DIR/4.4.207 /lib/modules
+	echo "Adding bluetooth packages to fix issues..."
+	apt install -qq pulseaudio-module-bluetooth bluez-firmware &> /dev/null
 
-	echo "Installing Chromium Default Flags for Improved Performance..."
-	mv $DIR/default /etc/chromium-browser
+	echo "Adding Community Wallpaper..."
+	cp $DIR/bgs/* /usr/share/backgrounds
 
 	echo
 	echo "Running Boot Partition Cleanup Script..."
